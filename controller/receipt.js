0cf21ed310d701be9ab70bc7fbfa2818ede03a6d
Shrim1812 <spaldhikar840@gmail.com>
2025-06-25 06:26:21 +0530

    Update receipt.js
    updated new code

--- controller/receipt.js
+++ controller/receipt.js
@@ -1,24 +1,8 @@
-import fs from "fs";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
-import PdfPrinter from "pdfmake";
+import pdfMake from 'pdfmake/build/pdfmake.js';
+import pdfFonts from 'pdfmake/build/vfs_fonts.js';
 import { poolPromise } from "../db.js";
 
-// __dirname for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// Fonts path
-const fonts = {
-  Roboto: {
-    normal: path.join(__dirname, "..", "fonts", "Roboto-Regular.ttf"),
-    bold: path.join(__dirname, "..", "fonts", "Roboto-Medium.ttf"),
-    italics: path.join(__dirname, "..", "fonts", "Roboto-Italic.ttf"),
-    bolditalics: path.join(__dirname, "..", "fonts", "Roboto-MediumItalic.ttf"),
-  },
-};
-
-const printer = new PdfPrinter(fonts);
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 export const ReceipPDF = async (req, res) => {
   try {
@@ -90,11 +74,13 @@ export const ReceipPDF = async (req, res) => {
       pageOrientation: "landscape",
     };
 
-    const pdfDoc = printer.createPdfKitDocument(docDefinition);
-    res.setHeader("Content-Type", "application/pdf");
-    res.setHeader("Content-Disposition", "inline; filename=payment-report.pdf");
-    pdfDoc.pipe(res);
-    pdfDoc.end();
+    const pdfDocGenerator = pdfMake.createPdf(docDefinition);
+    pdfDocGenerator.getBuffer((buffer) => {
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader("Content-Disposition", "inline; filename=payment-report.pdf");
+      res.send(buffer);
+    });
+
   } catch (err) {
     console.error("PDF generation error:", err);
     res.status(500).send("Error generating report");
