5e5d2f6a0d8fb865260e575a76c26f50b75c77a0
Shrim1812 <spaldhikar840@gmail.com>
2025-07-09 19:49:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const corsOptions = {
     credentials: true
 };
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+//app.options('*', cors(corsOptions));
 app.use("/Ohkla", MemberRouter);
 
 // PDF generation route
